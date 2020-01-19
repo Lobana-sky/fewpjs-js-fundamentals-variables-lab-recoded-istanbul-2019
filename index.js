f62b02558c03656da3b2cc8cc192f0c04af90d99
lobana alarabi <loubanaeng@gmail.com>
2020-01-19 22:40:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
+<<<<<<< HEAD
 const companyName = 'Scuber';
 companyName='lara';
 let mostProfitableNeighborhood='Chelsea';
 let companyCeo='Susan Smith';
+=======
+>>>>>>> 4949982840419c7cd766a1c5af4f11dfd82f1c12
