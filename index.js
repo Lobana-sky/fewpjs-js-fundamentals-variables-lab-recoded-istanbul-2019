ba329b8bc9d34afd8a0233cc1954533fc258f04e
lobana alarabi <loubanaeng@gmail.com>
2020-01-19 22:44:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
 
-const companyName = 'Scuber';
+const companyName = 'Scube';
 companyName='lara';
 let mostProfitableNeighborhood='Chelsea';
 let companyCeo='Susan Smith';
